751c63b8d55349e9d622af40553087d887d54c3d
Jerin Joseph <jerinjoseph@qburst.com>
2019-05-09 17:12:01 +0530

    Removed err usage

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ import (
 func main() {
 
 	idStr := "1"
-	id, _ := strconv.Atoi(idStr)
+	id, err := strconv.Atoi(idStr)
 	fmt.Println(id)
-	err:=first()
-fmt.Println(err)
+	first()
+	// fmt.Println(err)
 }
 func first() error {
-	return 			errors.New("error")
+	return errors.New("error")
 }
